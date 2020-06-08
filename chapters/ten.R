d700b16d34527444114baefab1bbff876721a06f
paulapivat <paul.apivat@gmail.com>
2020-06-08 10:10:08 +0700

    add ten.R file for Hypothesis Testing ch10

---
+++ chapters/ten.R
@@ -0,0 +1,15 @@
+# R version 3.6.3 (2020-02-29) -- "Holding the Windsock"
+# RStudio Version 1.2.5042
+
+#### Chapter 10 Hypothesis Testing ####
+
+# Packages
+library(tidyverse)
+library(moderndive)
+library(infer)
+library(patchwork)  # plot arrangement
+
+# load file
+load(file = 'moderndive.RData')
+
+
