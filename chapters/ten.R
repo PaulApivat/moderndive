9e6dd468d2fdaed181da29c3d45aa358a27fe4eb
paulapivat <paul.apivat@gmail.com>
2020-06-08 10:56:19 +0700

    exploratory analysis gender and promotions

--- chapters/ten.R
+++ chapters/ten.R
@@ -2,14 +2,40 @@
 # RStudio Version 1.2.5042
 
 #### Chapter 10 Hypothesis Testing ####
+## Computer-based methods using randomization, simulation and bootstrapping > Theory-based methods ###
+## Computational Thinking & Statistical Intuition
 
 # Packages
 library(tidyverse)
 library(moderndive)
 library(infer)
+library(nycflights13)
+library(ggplot2movies)
 library(patchwork)  # plot arrangement
 
 # load file
 load(file = 'moderndive.RData')
+
+##### Promotions Activity 
+## Data: Journal of Applied Psych (1974)
+## Does gender affect promotion decisions?
+
+# dataset: 3 columns, 48 rows
+promotions 
+
+# Randomly pick 6 rows; arranged by id
+promotions %>% 
+    sample_n(size = 6) %>% 
+    arrange(id)
+
+# Exploratory Data Analysis using Stacked Barchart
+# n = 24 resumes for each gender
+
+ggplot(data = promotions, mapping = aes(x = gender, fill = decision)) 
+    + geom_bar() 
+    + labs(x = "Gender of name on resume")
+
+
+
 
 
