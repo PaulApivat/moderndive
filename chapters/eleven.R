efe1272cec7233da09e478438f94a2da78a844dd
paulapivat <paul.apivat@gmail.com>
2020-06-10 16:46:39 +0700

    add Regression refresher

--- chapters/eleven.R
+++ chapters/eleven.R
@@ -3,7 +3,17 @@
 
 ### Chapter 11 Inference for Regression ###
 
-# Regression refresher: score ~ bty_avg
+# load file
+load(file = 'moderndive.RData')
+
+# packages
+library(tidyverse)
+library(moderndive)
+library(infer)
+library(patchwork)  # plot arrangement
+
+
+######## Regression refresher: score ~ bty_avg ###########
 # Sampling scenario around regression case; n = 463 is a sample
 # 463 teacher(s) assumed to be representative sample of greater population (e.g., all instructors at UT Austin)
 # (always need more knowledge of sampling methodology)
@@ -13,9 +23,47 @@
 # Hence, Scenario for sampling inference (population regression slope & fitted regression slope)
 
 # proceed to the rest of the Regression Table
-# std_error
-# statistic
-# p-value
+
+# STD_ERROR: if we collect from another 463 sample (score + bty_avg), another 1000 times, 
+# then put the data on histogram, we would have sampling distribution, standard deviation of sampling dist = std_error
+
+# statistic: (test statistic, 4.09) corresponds to standardized t-test statisic (theory-based, not computer-based)
+# note: get_regression_table is theory-based. SKIP FOR NOW
+
+# p-value: probability of obtaining a test statistic as extreme or more than observed test statistic (sample test statistic = 4.09)
+# assuming Null Hypothesis is true
+
 # lower_ci
 # upper_ci
 
+#### CONDITIONS FOR INFERENCE FOR REGRESSION #####
+## Do the following assumptions hold....
+
+# LINE
+# L - linearity of relationship between variables
+# I - independence of residuals
+# N - normality of residuals
+# E - equality of variance of residuals
+
+##### REFRESHER ON REGRESSION (moderndive package)
+evals # comes with moderndive package, dataset score, bty_avg (n = 463)
+
+evals_ch5 <- evals %>% select(ID, score, bty_avg, age)
+
+# Best Fitting Line through scatter plot
+ggplot(evals_ch5, aes(x = bty_avg, y = score)) 
+    + geom_point() 
+    + labs(x = 'Beauty Score', 
+           y = 'Teaching Score', 
+           title = 'Relationship between teaching and beauty scores') 
+    + geom_smooth(method = "lm", se = FALSE)
+
+# Fit Regression Model
+score_model <- lm(score ~ bty_avg, data = evals_ch5)
+
+# Get Regression Table
+get_regression_table(score_model)
+
+
+###### REFRESHER ON RESIDUALS
+###### SIMULTION-BASED INFERENCE FOR REGRESSION (instead of theory-based) ########
