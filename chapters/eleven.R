6aca1daa7d4419bc919e31b9aa393f1a9746dc34
paulapivat <paul.apivat@gmail.com>
2020-06-10 15:52:55 +0700

    add eleven.R with refresher on regression and link to sampling

---
+++ chapters/eleven.R
@@ -0,0 +1,21 @@
+# R version 3.6.3 (2020-02-29) -- "Holding the Windsock"
+# RStudio Version 1.2.5042
+
+### Chapter 11 Inference for Regression ###
+
+# Regression refresher: score ~ bty_avg
+# Sampling scenario around regression case; n = 463 is a sample
+# 463 teacher(s) assumed to be representative sample of greater population (e.g., all instructors at UT Austin)
+# (always need more knowledge of sampling methodology)
+# "estimate" in the regression table is a point estimate of population slope    (0.067)
+# "estimate" in the regression table is a point estimate of population intercept (3.880)
+# the "best fitting line" is an ESTIMATE of some true (and unknowable) POPULATION LINE
+# Hence, Scenario for sampling inference (population regression slope & fitted regression slope)
+
+# proceed to the rest of the Regression Table
+# std_error
+# statistic
+# p-value
+# lower_ci
+# upper_ci
+
